de6619962727a4c379baab74a44ee4084615ab15
rifqi <rifqhiyr@gmail.com>
2019-10-24 14:54:37 +0700

    [Alfianinda] - remove redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
